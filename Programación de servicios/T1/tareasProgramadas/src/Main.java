bfe527ee33a0cd8bb762d579c106b1dd8fc6b369
PedroMartz89 <pedromartinezsanchez89@gmail.com>
2025-11-03 20:38:22 +0100

    procesos

--- Programación de servicios/T1/tareasProgramadas/src/Main.java
+++ Programación de servicios/T1/tareasProgramadas/src/Main.java
@@ -1,5 +1,47 @@
+import javax.tools.OptionChecker;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Ejecutor gestorTareas = new Ejecutor();
+        Scanner sc = new Scanner(System.in);
+        gestorTareas.leerConfig("psptab.txt");
+
+
+    }
+
+    public void menu(Scanner sc, Ejecutor gestorTareas) {
+
+        while (true) {
+            System.out.println("Por favor, indica lo que quieres hacer: tareas, finalizar xx o salir.");
+            System.out.print("\n>");
+            String opcion = sc.nextLine();
+            if (!opcion.equals("tareas") && !opcion.equals("finalizar") && !opcion.equals("salir")) {
+                System.out.println("Introduce una opción válida.");
+            } else {
+
+                switch (opcion) {
+
+                    case "tareas" : {gestorTareas.listarTareas(); break;}
+                    case "finalizar": {
+
+                        System.out.println("Introduce el ID: ");
+                        sc.nextLine();
+                        int id = sc.nextInt();
+
+                        gestorTareas.cancelarTarea(id);
+                        break;
+                    }
+                    case "salir": break;
+                }
+                break;
+            }
+
+        }
+
+
+
 
     }
 }
--- Programación de servicios/T1/tareasProgramadas/src/Tarea.java
+++ Programación de servicios/T1/tareasProgramadas/src/Tarea.java
@@ -1,3 +1,83 @@
-public class Tarea {
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalTime;
 
+public class Tarea implements Runnable {
+    private int id;
+    private LocalTime horaEjecucion;
+    private String comando;
+    private String estado; // Pendiente, Ejecutado, Cancelado
+
+    public Tarea(int id, int hora,  int minutos, String comando, String estado) {
+        this.id = id;
+        this.horaEjecucion = LocalTime.of(hora, minutos);
+        this.comando = comando;
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id + ", Hora: " + horaEjecucion + ", Comando: " + comando + ", Estado: " + estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalTime getHoraEjecucion() {
+        return horaEjecucion;
+    }
+
+    public void setHoraEjecucion(LocalTime horaEjecucion) {
+        this.horaEjecucion = horaEjecucion;
+    }
+
+    public String getComando() {
+        return comando;
+    }
+
+    public void setComando(String comando) {
+        this.comando = comando;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            LocalTime ahora = LocalTime.now();
+            Duration duracion = Duration.between(ahora, horaEjecucion);
+            if (duracion.isNegative()) {
+                duracion = duracion.plusDays(1);
+            }
+
+            Thread.sleep(duracion.toMillis());
+            ejecutarComando(comando);
+            estado = "Ejecutado";
+
+        } catch (InterruptedException | IOException e) {
+            estado = "Cancelado";
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+    public void ejecutarComando(String comando) throws IOException, InterruptedException {
+
+        Process process = Runtime.getRuntime().exec(comando);
+        process.waitFor();
+        System.out.println("Comando ejecutado: " + comando);
+    }
 }
